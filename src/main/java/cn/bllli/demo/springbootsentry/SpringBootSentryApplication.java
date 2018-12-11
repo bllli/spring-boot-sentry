88bc50873c1b4905da3059d45ca4257262d0f4f6
bllli <lidengming1@100tal.com>
2018-12-11 13:04:18 +0800

    init

---
+++ src/main/java/cn/bllli/demo/springbootsentry/SpringBootSentryApplication.java
@@ -0,0 +1,12 @@
+package cn.bllli.demo.springbootsentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSentryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSentryApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/bllli/demo/springbootsentry/SpringBootSentryApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.bllli.demo.springbootsentry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSentryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
